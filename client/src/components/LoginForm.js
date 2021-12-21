8c1320730fa1b28d1a730c5309224458c2c5ac88
Eric Rion <erion42@gmail.com>
2021-12-20 21:29:57 -0500

    Login Page styled

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, Row, Col, Container } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
+import Wizard from '../components/img/DateKnightBig.png';
 
 import Auth from '../utils/auth';
 
@@ -57,29 +59,38 @@ const LoginForm = () => {
 
     return (
         <>
-            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
-                    Something went wrong with your login.
-                </Alert>
+            <Container fluid>
+                <Row>
+                    <div className="col-md-9">
+                        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
+                                Something went wrong with your login.
+                            </Alert>
 
-                <Form.Group>
-                    <Form.Label htmlFor="email">Email</Form.Label>
-                    <Form.Control type='text' placeholder="Your Email" name="email" onChange={handleInputChange} value={userFormData.email} required />
-                    <Form.Control.Feedback type="invalid">
-                        Email is required!
-                    </Form.Control.Feedback>
-                </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="email">Email</Form.Label>
+                                <Form.Control type='text' placeholder="Your Email" name="email" onChange={handleInputChange} value={userFormData.email} required />
+                                <Form.Control.Feedback type="invalid">
+                                    Email is required!
+                                </Form.Control.Feedback>
+                            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label htmlFor="password">Password</Form.Label>
-                    <Form.Control type="password" placeholder="Your Password" name="password" onChange={handleInputChange} value={userFormData.password} required />
-                    <Form.Control.Feedback type="invalid">
-                        Password is required!
-                    </Form.Control.Feedback>
-                </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="password">Password</Form.Label>
+                                <Form.Control type="password" placeholder="Your Password" name="password" onChange={handleInputChange} value={userFormData.password} required />
+                                <Form.Control.Feedback type="invalid">
+                                    Password is required!
+                                </Form.Control.Feedback>
+                            </Form.Group>
 
-                <Button disabled={!(userFormData.email && userFormData.password)} type="submit" variant="success">Submit!</Button>
-            </Form>
+                            <Button disabled={!(userFormData.email && userFormData.password)} type="submit" variant="primary">Submit!</Button>
+                        </Form>
+                    </div>
+                    <div className='col-md-3'>
+                        <img src= {Wizard} className='mx-auto'/>
+                    </div>
+                </Row>
+            </Container>
         </>
     );
 };
