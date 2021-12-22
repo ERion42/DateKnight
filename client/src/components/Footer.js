a437e6d5959a6f3f44348e5f6873b3d4bd03b450
Eric Rion <erion42@gmail.com>
2021-12-21 23:34:08 -0500

    Made footer pretty much only 'copyright' line, can add more later

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,13 +14,13 @@ const Footer = () => {
                 <a className="navbar-brand" href="/">
                    
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li> */}
+                        <li className="nav-item">
+                            <span className="nav-link">Copyright 2022</span>
                         </li>
                         {/* <li className="nav-item">
                             <a className="nav-link" href="/">Link</a>
